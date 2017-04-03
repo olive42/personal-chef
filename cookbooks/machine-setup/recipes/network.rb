ffb28d64124c7292448706c7737669f0d7dbe08e
Olivier Tharan <olivier@tharan.org>
2017-04-03 22:52:02 +0200

    Fix SHA256 sum for dhclient6.conf

--- cookbooks/machine-setup/recipes/network.rb
+++ cookbooks/machine-setup/recipes/network.rb
@@ -2,6 +2,6 @@ cookbook_file '/etc/dhcp/dhclient6.conf' do
   group 'root'
   owner 'root'
   mode '0444'
-  checksum '6b307b0fcce1008cc9d4d2af2d68aa313c8fee548bad2bb00da675a0226e2abe'
+  checksum '944d3bb1f9d55e33f02ddec9656f56552fd912ea31cc92cce29d4d666b5dec68'
   source 'dhclient6.conf'
 end
