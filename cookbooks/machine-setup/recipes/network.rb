9be7f09084f7ec111d0fb6444bd08382934bed38
Olivier Tharan <olivier@tharan.org>
2017-04-05 23:45:17 +0200

    Add service to take care of dhclient with IPv6
    Service enabling and start is commented out for now until tests are
    fixed.

--- cookbooks/machine-setup/recipes/network.rb
+++ cookbooks/machine-setup/recipes/network.rb
@@ -5,3 +5,15 @@ cookbook_file '/etc/dhcp/dhclient6.conf' do
   checksum '944d3bb1f9d55e33f02ddec9656f56552fd912ea31cc92cce29d4d666b5dec68'
   source 'dhclient6.conf'
 end
+
+cookbook_file '/etc/systemd/system/dhclient6.service' do
+  group 'root'
+  owner 'root'
+  mode '0444'
+  checksum '06b32746b9fef74af9f63cce190dd840495ec8fba33eca2288d976623b542de6'
+  source 'dhclient6.service'
+end
+
+# service 'dhclient6' do
+#   action [ :enable, :start ]
+# end
