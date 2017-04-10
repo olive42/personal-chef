ca071834722b69371383f883f5de9d5166a2df1b
Olivier Tharan <olivier@tharan.org>
2017-04-10 22:19:51 +0200

    Fix bug in package list

--- cookbooks/machine-setup/attributes/default.rb
+++ cookbooks/machine-setup/attributes/default.rb
@@ -1,3 +1,3 @@
 # Keep it sorted
-default['package_list'] = %w(fail2ban git ipcalc irssi pass tcptraceroute tmux \
-traceroute whois zsh)
+default['package_list'] = %w(fail2ban git ipcalc irssi pass tcptraceroute tmux
+                             traceroute whois zsh)
