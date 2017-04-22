ecba932af8ce3232e6162fbdfebeb65f7c2fe148
Olivier Tharan <olivier@tharan.org>
2017-04-22 10:08:50 +0200

    Update Policyfile after running chef generate policy

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,7 +1,18 @@
-name 'personal-chef'
-run_list 'machine-setup', 'user-files'
+# Policyfile.rb - Describe how you want Chef to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://docs.chef.io/policyfile.html
+
+# A name that describes what the system you're building with Chef does.
+name "personal-chef"
+
+# Where to find external cookbooks:
 default_source :supermarket
 
-cookbook 'chef-vault'
+# run_list: chef-client will run these recipes in the order specified.
+run_list "machine-setup::default", "user-files::default"
+
+# Specify a custom source for a single cookbook:
+# cookbook "example_cookbook", path: "../cookbooks/example_cookbook"
 cookbook 'machine-setup', path: "cookbooks/machine-setup"
 cookbook 'user-files', path: "cookbooks/user-files"
