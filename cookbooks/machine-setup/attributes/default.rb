419ff9725f3952a22e3734062ce7af20c832e93d
Olivier Tharan <olivier@tharan.org>
2017-04-26 22:39:45 +0200

    Update network configuration to use templates
    DHCPv6 now uses variables for:
    - interface name (so we can override it in Kitchen tests)
    - DUID stored in a secret vault
    
    TODO: add tests, enable service

--- cookbooks/machine-setup/attributes/default.rb
+++ cookbooks/machine-setup/attributes/default.rb
@@ -1,3 +1,6 @@
 # Keep it sorted
 default['package_list'] = %w[fail2ban git ipcalc irssi pass tcptraceroute tmux
                              traceroute whois zsh]
+
+# This should be overridable in kitchen test
+default['machine']['default_interface'] = 'enp0s20'
--- cookbooks/machine-setup/recipes/network.rb
+++ cookbooks/machine-setup/recipes/network.rb
@@ -1,17 +1,33 @@
-cookbook_file '/etc/dhcp/dhclient6.conf' do
+include_recipe 'chef-vault'
+vault = chef_vault_item(:credentials, 'serversecret')
+
+template '/etc/dhcp/dhclient6.conf' do
   group 'root'
   owner 'root'
   mode '0444'
   checksum '944d3bb1f9d55e33f02ddec9656f56552fd912ea31cc92cce29d4d666b5dec68'
-  source 'dhclient6.conf'
+  source 'dhclient6.conf.erb'
+  variables(
+    interface: node['machine']['default_interface']
+    duid: vault['duid']
+  )
 end
 
-cookbook_file '/etc/systemd/system/dhclient6.service' do
+template '/etc/systemd/system/dhclient6.service' do
   group 'root'
   owner 'root'
   mode '0444'
   checksum '06b32746b9fef74af9f63cce190dd840495ec8fba33eca2288d976623b542de6'
-  source 'dhclient6.service'
+  source 'dhclient6.service.erb'
+  variables(
+    interface: node['machine']['default_interface']
+  )
+  notifies :run, 'execute[systemctl-daemon-reload]', :immediately
+end
+
+execute 'systemctl-daemon-reload' do
+  command '/sbin/systemctl --system daemon-reload'
+  action :nothing
 end
 
 # service 'dhclient6' do
