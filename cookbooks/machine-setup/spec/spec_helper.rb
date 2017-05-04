7ed1b7845521ebef45f6f9ea78376b912a73f4bd
Olivier Tharan <olivier@tharan.org>
2017-05-04 22:05:03 +0200

    Update unittests for machine-setup
    Use chef-solo
    Use Ubuntu 16.04 (LTS)
    Test installation of a package

--- cookbooks/machine-setup/spec/spec_helper.rb
+++ cookbooks/machine-setup/spec/spec_helper.rb
@@ -1,1 +1,6 @@
 require 'chefspec'
+require 'chefspec/policyfile'
+
+set :backend, :exec
+
+ChefSpec::Coverage.start!
--- cookbooks/machine-setup/spec/unit/recipes/default_spec.rb
+++ cookbooks/machine-setup/spec/unit/recipes/default_spec.rb
@@ -27,14 +27,18 @@
 require 'spec_helper'
 
 describe 'machine-setup::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When all attributes are default, on an Ubuntu platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'installs git' do
+      expect(chef_run).to install_package('git')
     end
+
+    #it 'converges successfully' do
+    #  expect { chef_run }.to_not raise_error
+    #end
   end
 end
