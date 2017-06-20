b60b6e2673d69c1b3ba27feda1a69fa5fc9a3566
Olivier Tharan <o.tharan@criteo.com>
2017-06-20 17:18:37 +0200

    Rename policy for no particular reason
    Something something about an error during bootstrap with a '-' in a string.

--- Policyfile.rb
+++ Policyfile.rb
@@ -4,7 +4,7 @@
 # https://docs.chef.io/policyfile.html
 
 # A name that describes what the system you're building with Chef does.
-name 'personal-chef'
+name 'personalchef'
 
 # Where to find external cookbooks:
 default_source :supermarket
