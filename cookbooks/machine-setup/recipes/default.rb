940ea682e1231be22d9d5d5258f8d1037e436f66
Olivier Tharan <o.tharan@criteo.com>
2017-10-17 13:21:55 +0200

    Add support for git repositories

--- cookbooks/machine-setup/recipes/default.rb
+++ cookbooks/machine-setup/recipes/default.rb
@@ -10,7 +10,28 @@
 # Install packages
 package node['package_list']
 
+# Setup network
 include_recipe 'machine-setup::network'
+
+# Git repositories
+group 'git' do
+  system true
+end
+
+user 'git' do
+  comment 'Git user'
+  gid 'git'
+  home '/var/opt/git'
+  shell '/etc/noshell'
+  system true
+end
+
+directory '/var/opt/git' do
+  owner 'git'
+  mode '0755'
+end
+
+# Nginx
 include_recipe 'nginx'
 nginx_site 'bouffe.tharan.org' do
   template 'bouffe.tharan.org.conf.erb'
